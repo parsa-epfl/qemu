f51eebc0d84bcd30482bcd124eaaa8b67d7bfad2
Bryan Perdrizat <bryan.perdrizat@epfl.ch>
2024-02-20 08:42:25 +0100

    feat: add savevm-external build system
    This revert the previous build system, while as well adding a more
    streamlined way to compile futur extra-module for a simulator.
    The goal was to move as musch as code into the libqflex's repo,
    and leaving QEMU with the minimal possible invasive code

--- scripts/meson-buildoptions.sh
+++ scripts/meson-buildoptions.sh
@@ -156,6 +156,8 @@ meson_options_help() {
   printf "%s\n" '  rbd             Ceph block device driver'
   printf "%s\n" '  rdma            Enable RDMA-based migration'
   printf "%s\n" '  replication     replication support'
+  printf "%s\n" '  rutabaga-gfx    rutabaga_gfx support'
+  printf "%s\n" '  savevm-external Support for external snapshot'
   printf "%s\n" '  sdl             SDL user interface'
   printf "%s\n" '  sdl-image       SDL Image support for icons'
   printf "%s\n" '  seccomp         seccomp support'
@@ -429,6 +431,8 @@ _meson_option_parse() {
     --disable-safe-stack) printf "%s" -Dsafe_stack=false ;;
     --enable-sanitizers) printf "%s" -Dsanitizers=true ;;
     --disable-sanitizers) printf "%s" -Dsanitizers=false ;;
+    --enable-savevm-external) printf "%s" -Dsavevm-external=enabled ;;
+    --disable-savevm-external) printf "%s" -Dsavevm-external=disabled ;;
     --enable-sdl) printf "%s" -Dsdl=enabled ;;
     --disable-sdl) printf "%s" -Dsdl=disabled ;;
     --enable-sdl-image) printf "%s" -Dsdl_image=enabled ;;
