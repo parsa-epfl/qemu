381317b8c6718e6c34da73c2f6d772699b244bcf
Bryan Perdrizat <bryan.perdrizat@epfl.ch>
2024-03-06 18:00:38 +0100

    savevm-external: add overlay on drive load
    Add Invokation option
    Add build dependencies
    
    When loading an image with the -savevm-external flag enabled, QEMU will create a temporary image to apply the modification to. The image will have for backing file the main root ones.

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -139,6 +139,11 @@
 #include "middleware/libqflex/libqflex-module.h"
 #endif
 
+#ifdef CONFIG_SNAPVM_EXT
+#include "middleware/savevm-external/snapvm-external.h"
+#endif
+
+
 #define MAX_VIRTIO_CONSOLES 1
 
 typedef struct BlockdevOptionsQueueEntry {
@@ -646,6 +651,14 @@ static int cleanup_add_fd(void *opaque, QemuOpts *opts, Error **errp)
 
 static int drive_init_func(void *opaque, QemuOpts *opts, Error **errp)
 {
+
+#ifdef CONFIG_SNAPVM_EXT
+    if (qemu_snapvm_ext_state.is_enabled)
+    {
+        snapvm_init(opts, loadvm, errp);
+    }
+#endif
+
     BlockInterfaceType *block_default_type = opaque;
 
     return drive_new(opts, *block_default_type, errp) == NULL;
@@ -3588,7 +3601,8 @@ void qemu_init(int argc, char **argv)
 #endif /* CONFIG_LIBQFLEX */
 #ifdef CONFIG_SAVEVM_EXT
             case QEMU_OPTION_savevm_external:
-                qemu_log("savevm-external enabled");
+		        qemu_snapvm_ext_state.is_enabled = true;
+                break;
                 break;
 #endif
 
